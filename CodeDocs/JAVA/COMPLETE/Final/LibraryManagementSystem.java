bb85a95d501040fc0b20ba32c89835826554831f
Narmadhamohan <Narmadha.mohan1@gmail.com>
2024-09-25 20:38:19 +0530

    Create LibraryManagementSystem.java

---
+++ CodeDocs/JAVA/COMPLETE/Final/LibraryManagementSystem.java
@@ -0,0 +1,10 @@
+// Main class
+public class LibraryManagementSystem {
+    public static void main(String[] args) {
+        // Create an instance of the Book class
+        Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565");
+        
+        // Display the book details
+        book.displayDetails();
+    }
+}
