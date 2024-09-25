6ced6d1d99ecd7a273e2bb051205a83d1a697ae5
Narmadhamohan <Narmadha.mohan1@gmail.com>
2024-09-25 21:28:38 +0530

    Create MathUtils.java

---
+++ CodeDocs/JAVA/COMPLETE/NestedClass/MathUtils.java
@@ -0,0 +1,8 @@
+public class MathUtils {
+    public static class Constants {
+        public static final double PI = 3.14159;  // Static nested class holding a constant
+    }
+}
+
+// Usage
+//double circleArea = MathUtils.Constants.PI * radius * radius;  // Accessing constant
